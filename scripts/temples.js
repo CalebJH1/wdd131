62c6b0b94a01ce93b8a0ccb7f63904ba6546884e
CalebJH1 <cjheilmann@outlook.com>
2024-11-16 18:06:00 -0800

    commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,8 @@
-const year = document.querySelector("#currentyear");
-const date = document.querySelector("#lastModified");
+// const year = document.querySelector("#currentyear");
+// const date = document.querySelector("#lastModified");
 
-let lastModified = new Date(document.lastModified);
-const today = new Date();
+// let lastModified = new Date(document.lastModified);
+// const today = new Date();
 
-year.innerHTML = today.getFullYear();
-date.innerHTML = `Last Modified: ${new Intl.DateTimeFormat("en-US",{dateStyle: "short"}).format(lastModified)} ${new Intl.DateTimeFormat("en-US",{timeStyle: "long"}).format(lastModified)}`;
+// year.innerHTML = today.getFullYear();
+// date.innerHTML = `Last Modified: ${new Intl.DateTimeFormat("en-US",{dateStyle: "short"}).format(lastModified)} ${new Intl.DateTimeFormat("en-US",{timeStyle: "long"}).format(lastModified)}`;
