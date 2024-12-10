aace55344f98590f7a292ba45fd9fb5eab3260ee
CalebJH1 <cjheilmann@outlook.com>
2024-12-10 15:31:58 -0800

    commit

---
+++ scripts/review.js
@@ -0,0 +1,14 @@
+let timesLoaded = JSON.parse(window.localStorage.getItem('times-loaded')) || 0
+let mainElement = document.querySelector('main');
+
+window.addEventListener("load", () => {
+    if (window.history.length === 2)
+    {
+        timesLoaded++;
+        window.localStorage.setItem('times-loaded', JSON.stringify(timesLoaded));
+    }
+    
+    const paragraph = document.createElement("p");
+    paragraph.textContent = `Thank you for your response. You have submitted ${timesLoaded} responses.`
+    mainElement.appendChild(paragraph);
+})
